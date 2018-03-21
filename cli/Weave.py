7f33a8af5e5c218f81a5a50fc4d1f26033508546
Tarn Burton <twburton@gmail.com>
2018-03-21 08:17:30 -0400

    Add Weave function

---
+++ cli/Weave.py
@@ -0,0 +1,54 @@
+import Processors
+from Chunk import Chunk
+from Formatter import LaTeXFormatter, MarkDownFormatter
+import os
+
+
+def Weave(input, options=None):
+    dir, base = os.path.split(input)
+    if dir != '':
+        os.chdir(dir)
+
+    opts = {
+        'code_env': 'verbatim',
+        'echo': True,
+        'evaluate': True,
+        'expand_options': False,
+        'figure_env': 'figure',
+        'formatters': {
+            'latex': LaTeXFormatter(),
+            'markdown': MarkDownFormatter()
+        },
+        'inline': False,
+        'input': base,
+        'math_env': 'equation',
+        'name': 'doc',
+        'results': True,
+        'wrap_math': True
+    }
+
+    if options is not None:
+        opts.update(options)
+
+    doc = Chunk(type='group', options=opts)
+
+    with Processors.ParseInput(doc) as p:
+        p.apply()
+
+    with Processors.DeduceOptions(doc) as p:
+        p.apply()
+
+    with Processors.NameChunks(doc) as p:
+        p.apply()
+
+    with Processors.ApplyDefaultOptions(doc) as p:
+        p.apply()
+
+    with Processors.EvaluateCode(doc) as p:
+        p.apply()
+
+    with Processors.FormatOutput(doc) as p:
+        p.apply()
+
+    with Processors.WriteOutput(doc) as p:
+        p.apply()
--- cli/main.py
+++ cli/main.py
@@ -1,8 +1,5 @@
-import Processors
 import argparse
-from Chunk import Chunk
-from Formatter import LaTeXFormatter, MarkDownFormatter
-import os
+from Weave import Weave
 
 
 ap = argparse.ArgumentParser()
@@ -13,51 +10,13 @@ ap.add_argument('--kernel')
 ap.add_argument('--parser')
 args = ap.parse_args()
 
-dir, input = os.path.split(args.input)
-os.chdir(dir)
-
-doc = Chunk(type='group', options={
-    'code_env': 'verbatim',
-    'echo': True,
-    'evaluate': True,
-    'expand_options': False,
-    'figure_env': 'figure',
-    'formatters': {
-        'latex': LaTeXFormatter(),
-        'markdown': MarkDownFormatter()
-    },
-    'inline': False,
-    'input': input,
-    'math_env': 'equation',
-    'name': 'doc',
-    'results': True,
-    'wrap_math': True
-})
+options = {}
 
 if args.kernel is not None:
-    doc.options['kernel'] = args.kernel
+    options['kernel'] = args.kernel
 if args.parser is not None:
-    doc.options['parser'] = args.parser
+    options['parser'] = args.parser
 if args.format is not None:
-    doc.options['format'] = args.format
+    options['format'] = args.format
 
-with Processors.ParseInput(doc) as p:
-    p.apply()
-
-with Processors.DeduceOptions(doc) as p:
-    p.apply()
-
-with Processors.NameChunks(doc) as p:
-    p.apply()
-
-with Processors.ApplyDefaultOptions(doc) as p:
-    p.apply()
-
-with Processors.EvaluateCode(doc) as p:
-    p.apply()
-
-with Processors.FormatOutput(doc) as p:
-    p.apply()
-
-with Processors.WriteOutput(doc) as p:
-    p.apply()
+Weave(args.input, options)
